ebede9ab02f7236398ccf1e31c2bce1ee3da2c12
Yuriy Susuk <yuriy.susuk@scout24.com>
2017-08-07 09:04:01 +0200

    will take part by part

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,15 +1,52 @@
-import scala.util.Random
+import scala.annotation.tailrec
 
 object Main extends App {
 
-  def find(fst: String, snd: String): String = {
-    fst.split("/").zip(snd.split("/")).zipWithIndex.map { el =>
+  println(DynamicPartsNaive.find("/users/Ben/info/location", "/users/Lewis/info/birthday"))
+  println(DynamicParts.find("/users/Ben/info/location", "/users/Lewis/info/birthday"))
+}
+
+trait DynamicParts {
+  def find(path1: String, path2: String): String
+}
+
+object DynamicPartsNaive extends DynamicParts {
+  val random = scala.util.Random
+
+  def find(path1: String, path2: String): String = {
+    path1.split("/").zip(path2.split("/")).zipWithIndex.map { el =>
       el match {
         case ((str1, str2), _) if str1 == str2 => str1
-        case ((_, _), idx) => s"*query_${idx}_*"
+        case ((_, _), idx) => s"*query_${random.nextPrintableChar()}*"
       }
     }.mkString("/")
   }
 
-  println(find("/users/Ben/info/location", "/users/Lewis/info/birthday"))
+}
+
+// memory optimized
+object DynamicParts extends DynamicParts {
+  val random = scala.util.Random
+
+  def find(path1: String, path2: String): String = {
+    @tailrec
+    def innerFind(path1: String, path2: String, accum: String): String = {
+      if (path1.isEmpty)
+        accum
+      else {
+        val (headFromPath1, tailFromPath1) = path1.span(_ != '/')
+        val (headFromPath2, tailFromPath2) = path2.span(_ != '/')
+
+        val path = (headFromPath1, headFromPath2) match {
+          case (fromPath1, fromPath2) if fromPath1 == fromPath2 => fromPath1
+          case (_, _) => s"*query_${random.nextPrintableChar()}*"
+        }
+
+        innerFind(tailFromPath1.drop(1), tailFromPath2.drop(1), accum ++ "/" ++ path)
+      }
+    }
+
+    innerFind(path1.drop(1), path2.drop(1), "")
+  }
+
 }
